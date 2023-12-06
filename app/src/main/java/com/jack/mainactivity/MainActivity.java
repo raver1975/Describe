4aecc571d25858fb83eecf760779b34cbad8125f
Paul Klemstine <paulklemstine@gmail.com>
2023-12-05 21:25:34 -0600

    llava integration

--- app/src/main/java/com/jack/mainactivity/MainActivity.java
+++ app/src/main/java/com/jack/mainactivity/MainActivity.java
@@ -2,7 +2,11 @@ package com.jack.mainactivity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
@@ -16,34 +20,32 @@ import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.HandlerThread;
+import android.net.Uri;
+import android.os.*;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import android.view.Window;
+import android.widget.*;
+
+import java.io.*;
+import java.lang.Process;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "AndroidCameraApi";
     private Button takePictureButton;
+    private Button describeButton;
     private TextureView textureView;
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
 
@@ -66,22 +68,242 @@ public class MainActivity extends AppCompatActivity {
     private boolean mFlashSupported;
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
+    private TextView promptText;
+    private TextView errorText;
+    private TextView predictionText;
+    private ScrollView errorScroll;
+    private ScrollView predictionScroll;
+    private ImageView imageView;
+
+    public static String getStringFromFile(String filePath) throws Exception {
+        File fl = new File(filePath);
+        if (!fl.exists()) return null;
+        FileInputStream fin = new FileInputStream(fl);
+        String ret = convertStreamToString(fin);
+        //Make sure you close all streams.
+        fin.close();
+        return ret;
+    }
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         textureView = (TextureView) findViewById(R.id.texture);
         assert textureView != null;
         textureView.setSurfaceTextureListener(textureListener);
         takePictureButton = (Button) findViewById(R.id.btn_takepicture);
+        describeButton = (Button) findViewById(R.id.btn_describe);
+        promptText = (TextView) findViewById(R.id.prompt);
+        errorText = (TextView) findViewById(R.id.errorText);
+        predictionText = (TextView) findViewById(R.id.predictionText);
+        errorText.setMovementMethod(new ScrollingMovementMethod());
+        predictionText.setMovementMethod(new ScrollingMovementMethod());
+        errorScroll = (ScrollView) this.findViewById(R.id.errorScroll);
+        predictionScroll = (ScrollView) this.findViewById(R.id.predictionScroll);
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+//        Uri uri=Uri.fromFile(new File(Uri.fromFile(new ContextWrapper(MainActivity.this).getExternalFilesDir("Documents")).toString() + "pic.jpg"));
+//        imageView.setImageURI(uri);
+        imageView.setImageResource(R.drawable.ic_launcher_background);
+
         assert takePictureButton != null;
+        assert describeButton != null;
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.MANAGE_EXTERNAL_STORAGE
+
+                }, 1
+        );
+        if (Build.VERSION.SDK_INT >= 30) {
+            if (!Environment.isExternalStorageManager()) {
+                Intent getpermission = new Intent();
+                getpermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                startActivity(getpermission);
+            }
+        }
+
+//        Handler mainHandler = new Handler(new ContextWrapper(MainActivity.this).getMainLooper());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            File sdcard = Environment.getExternalStoragePublicDirectory("Documents");
+                            try {
+                                errorText.setText(getStringFromFile(sdcard.getPath() + "/llavaerror.txt"));
+                                errorScroll.post(new Runnable() {
+                                    public void run() {
+                                        errorScroll.fullScroll(View.FOCUS_DOWN);
+                                    }
+                                });
+
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                            try {
+                                predictionText.setText(getStringFromFile(sdcard.getPath() + "/llava.txt"));
+                                predictionScroll.post(new Runnable() {
+                                    public void run() {
+                                        predictionScroll.fullScroll(View.FOCUS_DOWN);
+                                    }
+                                });
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                            File ff=Environment.getExternalStoragePublicDirectory("Documents");
+                            File f=new File(ff.getPath()+File.separator+"pic.jpg");
+                            Bitmap bmp = BitmapFactory.decodeFile(f.getPath());
+                            Log.d("describe","f="+f+" "+f.exists()+" "+bmp);
+//        imageView.setImageBitmap(decodeSampledBitmapFromFile(f.getAbsolutePath(), 200, 200));
+                            imageView.setImageBitmap(bmp);
+                        }
+                    });
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                    }
+
+                }
+
+            }
+        }).start();
+
+        describeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePrompt();
+            }
+        });
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 takePicture();
             }
         });
+
+//         Unpacking script to local filesystem
+//        String pathToScript1 = getDir("my_scripts", 0).getAbsolutePath() + File.separator + "llavacli";
+//        String pathToScript2 = getDir("my_scripts", 0).getAbsolutePath() + File.separator + "run.sh";
+
+//        InputStream in = getResources().openRawResource(R.raw.llavacli);
+//        FileOutputStream out = null;
+//        try {
+//            out = new FileOutputStream(pathToScript1);
+//            byte[] buff = new byte[1024];
+//            int read = 0;
+//            while ((read = in.read(buff)) > 0) {
+//                out.write(buff, 0, read);
+//            }
+//        } catch (Exception e) {
+//        } finally {
+//            try {
+//                in.close();
+//                out.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        try {
+//            out.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        in = getResources().openRawResource(R.raw.run);
+//        try {
+//            out = new FileOutputStream(pathToScript2);
+//            byte[] buff = new byte[1024];
+//            int read = 0;
+//            while ((read = in.read(buff)) > 0) {
+//                out.write(buff, 0, read);
+//            }
+//        } catch (Exception e) {
+//        } finally {
+//            try {
+//                in.close();
+//                out.close();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        // Make script executable
+
+        // Execute script
+
+    }
+    public static Bitmap decodeSampledBitmapFromFile(String path,
+                                                     int reqWidth, int reqHeight) { // BEST QUALITY MATCH
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        // Calculate inSampleSize
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        int inSampleSize = 1;
+
+        if (height > reqHeight) {
+            inSampleSize = Math.round((float)height / (float)reqHeight);
+        }
+
+        int expectedWidth = width / inSampleSize;
+
+        if (expectedWidth > reqWidth) {
+            //if(Math.round((float)width / (float)reqWidth) > inSampleSize) // If bigger SampSize..
+            inSampleSize = Math.round((float)width / (float)reqWidth);
+        }
+
+
+        options.inSampleSize = inSampleSize;
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(path, options);
+    }
+    private void takePrompt() {
+        final File file = new File(Environment.getExternalStorageDirectory() + "/Documents/prompt.txt");
+        Log.d("describe", "saving prompt at " + file.getPath());
+        try {
+            save(file, promptText.getText().toString().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void save(File file1, byte[] bytes) throws IOException {
+        OutputStream output = null;
+        try {
+            output = new FileOutputStream(file1);
+            output.write(bytes);
+        } finally {
+            if (null != output) {
+                output.close();
+            }
+        }
     }
 
     TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
@@ -163,8 +385,15 @@ public class MainActivity extends AppCompatActivity {
             if (characteristics != null) {
                 jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
             }
-            int width = 640;
-            int height = 480;
+            StreamConfigurationMap map = characteristics.get(
+                    CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+            Size smallest = Collections.max(
+                    Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                    new CompareSizesByArea());
+            int width = smallest.getWidth();
+            int height = smallest.getHeight();
+            Log.d("describe", width + "," + height);
             if (jpegSizes != null && 0 < jpegSizes.length) {
                 width = jpegSizes[0].getWidth();
                 height = jpegSizes[0].getHeight();
@@ -179,7 +408,7 @@ public class MainActivity extends AppCompatActivity {
             // Orientation
             int rotation = getWindowManager().getDefaultDisplay().getRotation();
             captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
-            final File file = new File(Environment.getExternalStorageDirectory() + "/pic.jpg");
+            final File file = new File(Environment.getExternalStorageDirectory() + "/Documents/pic.jpg");
             ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
                 @Override
                 public void onImageAvailable(ImageReader reader) {
@@ -219,6 +448,16 @@ public class MainActivity extends AppCompatActivity {
                 public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
                     super.onCaptureCompleted(session, request, result);
                     Toast.makeText(MainActivity.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
+
+                    Log.d("describe", "process starting");
+
+//                    String pathToScript1 = getDir("my_scripts", 0).getAbsolutePath() + File.separator + "llavacli";
+//                    String pathToScript2 = getDir("my_scripts", 0).getAbsolutePath() + File.separator + "run.sh";
+//                    executeCommand("chmod +x " + pathToScript1);
+//                    executeCommand("chmod +x " + pathToScript2);
+//                    executeCommand("sh "+pathToScript2);
+//                    executeCommand("sh storage/emulated/0/llava/run.sh");
+
                     createCameraPreview();
                 }
             };
@@ -239,6 +478,25 @@ public class MainActivity extends AppCompatActivity {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private void logprocess(Process process) throws IOException {
+        String str = null;
+        BufferedReader bufRd = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        while ((str = bufRd.readLine()) != null) {
+            Log.d("describe", str);
+            sb.append(str + "\n");
+        }
+        str = sb.toString();
+        Log.d("describe", str);
+        BufferedReader bufRd1 = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        sb = new StringBuilder();
+        while ((str = bufRd1.readLine()) != null) {
+            Log.d("describe", str);
+            sb.append(str + "\n");
+        }
+        str = sb.toString();
     }
 
     protected void createCameraPreview() {
@@ -345,4 +603,78 @@ public class MainActivity extends AppCompatActivity {
         stopBackgroundThread();
         super.onPause();
     }
+
+
+    public static void executeCommandAndGetOutput(String command) {
+        BufferedReader reader = null;
+        String result = "";
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                result += line + "\n";
+            }
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null)
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+        Log.i("Test", result);
+    }
+
+    private void executeCommand(String s) {
+
+        try {
+            Log.d("describe", s);
+            Process process = Runtime.getRuntime().exec(s);
+            logprocess(process);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getSystemCommandOutput(String command) {
+        BufferedReader reader = null;
+        String result = "";
+
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                result += line + "\n";
+            }
+
+            p.waitFor();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
+
+/**
+ * Compares two {@code Size}s based on their areas.
+ */
+class CompareSizesByArea implements Comparator<Size> {
+
+    @Override
+    public int compare(Size lhs, Size rhs) {
+        // We cast here to ensure the multiplications won't overflow
+        return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+                (long) rhs.getWidth() * rhs.getHeight());
+    }
+
+}
+
+
