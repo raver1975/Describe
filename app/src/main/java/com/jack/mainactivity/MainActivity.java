2b5da1f1c93cd0ea9b22583dd391f86989d5fd73
Paul Klemstine <paulklemstine@gmail.com>
2023-12-05 23:40:56 -0600

    remove title bar

--- app/src/main/java/com/jack/mainactivity/MainActivity.java
+++ app/src/main/java/com/jack/mainactivity/MainActivity.java
@@ -30,10 +30,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
-import android.view.Surface;
-import android.view.TextureView;
-import android.view.View;
-import android.view.Window;
+import android.view.*;
 import android.widget.*;
 
 import java.io.*;
@@ -99,7 +96,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         textureView = (TextureView) findViewById(R.id.texture);
         assert textureView != null;
